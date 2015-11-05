85dca2354d8a2fdb27572c706036eec9e0d08c3b
Joel Jacobson <joel@joeljacobson.com>
2015-11-05 09:40:08 +0000

    removed install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-# download dsc
-curl -L http://downloads.datastax.com/community/dsc-cassandra-2.2.3-bin.tar.gz | tar xz
-
-# download datastax-agent
-curl -L http://downloads.datastax.com/community/datastax-agent-\5.2.2.tar.gz | tar xz
-echo "stomp_interface: 127.0.0.1" >> ./datastax-agent-5.2.2/conf/address.yaml
-
-# download opscenter
-curl -L http://downloads.datastax.com/community/opscenter-5.2.2.tar.gz | tar xz
--- start-cassandra.sh
+++ start-cassandra.sh
@@ -1,8 +1,16 @@
 # checking if cassandra is present
 if [ ! -d dsc-cassandra-2.2.3 ] & [ ! -d datastax-agent-5.2.2 ] & [ ! -d opscenter-5.2.2 ] ; then
-    echo "files not found!, running install script"
-    ./install.sh
-fi
+    echo "files not found, grabbing paclages"
+    # download dsc
+    curl -L http://downloads.datastax.com/community/dsc-cassandra-2.2.3-bin.tar.gz | tar xz
+
+    # download datastax-agent
+    curl -L http://downloads.datastax.com/community/datastax-agent-\5.2.2.tar.gz | tar xz
+    echo "stomp_interface: 127.0.0.1" >> ./datastax-agent-5.2.2/conf/address.yaml
+
+    # download opscenter
+    curl -L http://downloads.datastax.com/community/opscenter-5.2.2.tar.gz | tar xz
+    fi
 
 # start the cassandra instance
 echo 'starting cassandra'
