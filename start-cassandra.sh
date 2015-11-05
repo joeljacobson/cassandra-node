59cf8438d964af3d26e0f5817b00616747c34476
Joel Jacobson <joel@joeljacobson.com>
2015-11-05 17:11:01 +0000

    bump

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -1,30 +1,34 @@
+VERSION="2.2.3"
+OPSCENTER="5.2.2"
+AGENT="5.2.2"
+
     # checking if cassandra is present
-    if [ ! -d dsc-cassandra-2.2.3 ] & [ ! -d datastax-agent-5.2.2 ] & [ ! -d opscenter-5.2.2 ] ; then
-    echo "files not found, grabbing packages"
+    if [ ! -d dsc-cassandra-$VERSION ] & [ ! -d datastax-agent-$AGENT ] & [ ! -d opscenter-$OPSCENTER ] ; then
+    echo "not found, grabbing packages"
 
     # download dsc
-    curl -L http://downloads.datastax.com/community/dsc-cassandra-2.2.3-bin.tar.gz | tar xz
+    curl -L http://downloads.datastax.com/community/dsc-cassandra-$VERSION-bin.tar.gz | tar xz
 
     # download datastax-agent
-    curl -L http://downloads.datastax.com/community/datastax-agent-\5.2.2.tar.gz | tar xz
-    echo "stomp_interface: 127.0.0.1" >> ./datastax-agent-5.2.2/conf/address.yaml
+    curl -L http://downloads.datastax.com/community/datastax-agent-$AGENT.tar.gz | tar xz
+    echo "stomp_interface: 127.0.0.1" >> ./datastax-agent-$AGENT/conf/address.yaml
 
     # download opscenter
-    curl -L http://downloads.datastax.com/community/opscenter-5.2.2.tar.gz | tar xz
+    curl -L http://downloads.datastax.com/community/opscenter-$OPSCENTER.tar.gz | tar xz
     fi
 
 # start the cassandra instance
 echo 'starting cassandra'
-dsc-cassandra-2.2.3/bin/cassandra start &> /dev/null
+dsc-cassandra-$VERSION/bin/cassandra start &> /dev/null
 sleep 4
 
 # start the datastax agent
 echo 'starting the datastax-agent'
-datastax-agent-5.2.2/bin/datastax-agent &> /dev/null
+datastax-agent-$AGENT/bin/datastax-agent &> /dev/null
 sleep 4
 
 # start opscenter
 echo 'starting opscenter'
-opscenter-5.2.2/bin/opscenter &> /dev/null
+opscenter-$OPSCENTER/bin/opscenter &> /dev/null
 sleep 3
 echo 'opscenter is running on 127.0.0.1:8888'
