ab63b228c60fba7bb8d74cba114ea73dcdc3ea41
Joel Jacobson <joel@joeljacobson.com>
2015-11-13 00:33:06 +0000

    open browser

--- start-cassandra.sh
+++ start-cassandra.sh
@@ -36,4 +36,5 @@ sleep 3
 echo 'adding node via opscenter api'
 curl -X POST http://127.0.0.1:8888/cluster-configs -d '{"cassandra": {"seed_hosts": "localhost"}, "cassandra_metrics": {}, "jmx": {"port": "7199"}}' &> /dev/null
 
-echo 'opscenter is running on 127.0.0.1:8888' 
+echo 'opscenter is running on 127.0.0.1:8888'
+python -mwebbrowser http://127.0.0.1:8888 
